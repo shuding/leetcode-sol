1799f40629abce5d9d8ec916c99ad0d5d4fa4b5c
Parabola <ds303077135@gmail.com>
2016-06-01 21:20:52 +0800

    Create sol.js

---
+++ algorithm/rotate-image/sol.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    var n = matrix.length, t;
+    for (var i = 0; i + i < n; ++i) {
+        for (var j = i; j < n - i - 1; ++j) {
+            t = matrix[i][j];
+            matrix[i][j] = matrix[n - j - 1][i];
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+            matrix[j][n - i - 1] = t;
+        }
+    }
+};
